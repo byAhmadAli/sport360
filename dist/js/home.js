11ab8cdb02b7b6ad5d59829db0aa32c9829ad9dd
Noura AlZboubi <nzoubi@cashU.local>
2017-08-23 00:43:23 +0300

    home page fix

--- dist/js/home.js
+++ dist/js/home.js
@@ -2,18 +2,20 @@ $(document).ready(function () {
     $("#sidebar").stick_in_parent({
         offset_top: $("#masthead").height()
     });
-    ao = "";
+    /*ao = "";
     ai = "";
     if ($(window).width() < 960) {
         ao = "slideOutUp";
         ai = "slideInUp";
-    }
+    }*/
+    var url = window.location.href.includes("/en");
     $('.results.owl-carousel').owlCarousel({
+        rtl: !url,
         loop: true,
         autoplay: true,
         nav: true,
-        animateOut: ao,
-        animateIn: ai,
+        /*animateOut: ao,
+        animateIn: ai,*/
         responsiveClass: true,
         responsive: {
             0: {
@@ -32,6 +34,7 @@ $(document).ready(function () {
         }
     });
     $('.owl-carousel').owlCarousel({
+        rtl: !url,
         loop: true,
         autoplay: true,
         nav: true,
@@ -52,8 +55,13 @@ $(document).ready(function () {
             }
         }
     })
-    $(".owl-prev").html('<i class="fa fa-chevron-left fa-2x"></i>');
-    $(".owl-next").html('<i class="fa fa-chevron-right fa-2x"></i>');
+    if(url){
+        $(".owl-prev").html('<i class="fa fa-chevron-left fa-2x"></i>');
+        $(".owl-next").html('<i class="fa fa-chevron-right fa-2x"></i>');
+    } else {
+        $(".owl-prev").html('<i class="fa fa-chevron-right fa-2x"></i>');
+        $(".owl-next").html('<i class="fa fa-chevron-left fa-2x"></i>');
+    }
     $("#toogle-details").click(function () {
         $(".matchdetils").toggle();
     });
