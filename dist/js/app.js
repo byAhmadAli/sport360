b1ebb133a29abc372727ec082f9368820356d7c1
ahmad ali <aali@ahmads-mbp.zte.com.cn>
2017-08-31 01:53:58 +0300

    update

--- dist/js/app.js
+++ dist/js/app.js
@@ -6,12 +6,59 @@ $(document).ready(function () {
     $('.card.big .author .avatar').hover(function() {
     	$(this).parent().find('.profile-details').toggleClass('show');
     });
- //    $('.slider').slick({
-	// 	dots: true,
-	// 	infinite: true,
-	// 	centerMode: true,
-	// 	centerPadding: '12%',
-	// 	slidesToShow: 3,
-	// 	speed: 500
-	// });
+    // slider
+    var slider = function(selector, show, scroll, arrows, repeat){
+        var slider = $(selector);
+        var direction = {}
+        direction.data = slider.data('direction');
+        direction.flag = false;
+        if(direction.data == "rtl"){
+            direction.flag = true;
+        }
+        slider.slick({
+            dots: true,
+            infinite: repeat,
+            speed: 300,
+            slidesToShow: show,
+            slidesToScroll: scroll,
+            rtl: direction.flag,
+            prevArrow: $(arrows[0]),
+            nextArrow: $(arrows[1]),
+            autoplay:false,
+            autoplaySpeed:5000,
+            responsive: [
+                {
+                    breakpoint: 769,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                    }
+                },
+                {
+                    breakpoint: 376,
+                    settings: "unslick"
+                }
+            ]
+        });
+        slider.animate({
+            opacity: 1
+        });
+    }
+    // big slider
+    var bigSlider = new slider('.big-slider', 3, 3, ['.big-prev', '.big-next'],  true);
+    var bigSlider = new slider('.promoted-slider', 3, 3, ['.promoted-prev', '.promoted-next'],  true);
+    
+
+    
+
+    // $('.post-action').each(function () {
+    //     var _this = this;
+    //   $('body').append($('.post-action').css({
+    //     position:'absolute',
+    //     left:$(_this).offset().left*-1, 
+    //     top:$(_this).offset().top,
+    //     'z-index': 99
+    //   }).detach());
+    // });
+    
 });
